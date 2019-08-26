c1cec84821f1b035db5d9a7c96babee0124a4198
wangyingbo <wangyingbo528@126.com>
2019-08-26 12:38:29 +0800

    add demo

---
+++ AUIAnimatedText-master/Project/AUIAnimatedText/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AUIAnimatedText
+//
+//  Created by Adam Siton on 9/20/11.
+//  Copyright 2011 Any.do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, nil);
+        return retVal;
+    }
+}
